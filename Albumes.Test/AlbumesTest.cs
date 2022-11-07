a45f9dc0728d8f33239fa3a950a758b873594758
Pedro Abad <pedrete01@gmail.com>
2022-11-07 10:57:50 +0100

    Tests

---
+++ Albumes.Test/AlbumesTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Albumes.Test
+{
+    public class AlbumesTest
+    {
+        [Fact]
+        public void crearAlbum_validarNombre()
+        {
+
+        }
+    }
+}
